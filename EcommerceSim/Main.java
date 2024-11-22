0d63192e8e5aac38b4cd85185ebb952942551dff
almira mirza <almira@mirza.com>
2024-11-23 01:44:50 +0300

    ecommence

---
+++ EcommerceSim/Main.java
@@ -0,0 +1,7 @@
+package EcommerceSim;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }    
+}
---
+++ EcommerceSim/Payment.java
@@ -0,0 +1,5 @@
+package EcommerceSim;
+
+public class Payment {
+    
+}
