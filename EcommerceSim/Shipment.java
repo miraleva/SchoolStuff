2284be0912962d616d2a3bc7e55dd1213d0c380e
almira mirza <almira@mirza.com>
2024-11-23 15:31:41 +0300

    shipping tamamlnadı

--- EcommerceSim/Shipment.java
+++ EcommerceSim/Shipment.java
@@ -1,5 +1,26 @@
 package EcommerceSim;
 
 public class Shipment {
+    private String adress;
+    private String shippingWay;
+    
+    public Shipment(String adress, String shippingWay){
+        this.adress=adress;
+        this.shippingWay=shippingWay;
+    }
+
+    public void shippingDetail(){
+        System.out.println("The adress is:"+ adress);
+        System.out.println("Shipping Method is:"+ shippingWay);
+
+    }
+
+    public String getAddress(){
+        return adress;
+    }
+    
+    public String getShippingWay(){
+        return shippingWay;
+    }
     
 }
--- EcommerceSim/ShoppingBasket.java
+++ EcommerceSim/ShoppingBasket.java
@@ -1,5 +1,30 @@
 package EcommerceSim;
 
 public class ShoppingBasket {
-    
+    private Product[] products;
+    private int[] quantities;
+    private int count;
+
+    public ShoppingBasket(){
+        products= new Product[5];
+        quantities= new int[5];
+        count= 0;
+
+    }
+    public void addProduct(){
+
+    }
+
+    public void removeProduct(){
+
+    }
+
+    public double getTotal(){
+
+    }
+
+    public void showBasket(){
+       
+        }
+    }
 }
