35733a1318eec682f8afc98c7d6f3e68fef54694
almira mirza <almira@mirza.com>
2024-11-23 15:48:14 +0300

    payment okey ya

--- EcommerceSim/Payment.java
+++ EcommerceSim/Payment.java
@@ -1,5 +1,19 @@
 package EcommerceSim;
 
 public class Payment {
+    private double total;
+
+    public Payment(double total){
+        this.total=total;
+    }
     
+    public void Pay(String paymentWay){
+        System.out.println("Paying total of:" + total + "with" + paymentWay);
+
+    }
+    
+    public double getTotal(){
+        return total;
+
 }
+
