d3082066927abc244b92defbc00c682444774a71
almira mirza <almira@mirza.com>
2024-12-26 23:43:52 +0300

    fr fin

--- ContactBstHw/Main.java
+++ ContactBstHw/Main.java
@@ -5,25 +5,25 @@ package ContactBstHw;
 public class Main {
     public static void main(String[] args) {
         Tree testTree = new Tree();
-        testTree.add("Mahmet", "123");
+        testTree.add("Fatih", "123");
         testTree.add("Ahmet", "234");
-        testTree.add("Zahmet", "456");
+        testTree.add("Zeliha", "456");
 
         System.out.println(testTree.root.name);
         System.out.println(testTree.root.left.name);
         System.out.println(testTree.root.right.name);
 
-        System.out.println(testTree.search("Zahmet"));
-        System.out.println(testTree.search("Cahmet"));
+        System.out.println(testTree.search("Zeliha"));
+        System.out.println(testTree.search("Cihan"));
 
-        testTree.add("Kahmet", "567");
+        testTree.add("Mehmet", "567");
         System.out.println(testTree.root.phone);
 
         testTree.show(testTree.root);
 
-        testTree.delete("Mahmet");
+        testTree.delete("Fatih");
 
-        System.out.println(testTree.search("Mahmet"));
+        System.out.println(testTree.search("Fatih"));
 
     }
 }
