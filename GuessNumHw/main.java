442edf68576312326d1e8bd408d019bb12cf7a32
almira mirza <miramirza74@gmail.com>
2025-01-17 22:58:50 +0300

    a

--- GuessNumHw/main.java
+++ GuessNumHw/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) throws Exception {
-        GuessNum game = new GuessNum(38);  //Sabit bir hedef sayı belirledik
+        GuessNum game = new GuessNum(38);  //Sabit bir hedef sayı belirledim
         game.startGame();
     }
 }
