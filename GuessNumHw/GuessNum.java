70463bee4a1821b059416c3630e032ab0f648ac4
almira mirza <miramirza74@gmail.com>
2025-01-17 22:56:50 +0300

    mini düzenleme

--- GuessNumHw/GuessNum.java
+++ GuessNumHw/GuessNum.java
@@ -23,20 +23,28 @@ class GuessNum {
             while ((c = System.in.read()) != '\n') {
                 if (c >= '0' && c <= '9') {
                     guess = guess * 10 + (c - '0');
+                } else if (c == '-' && guess == 0) { 
+                    guess = -1;
+                    break;
                 } else {
                     validInput = false;
                 }
             }
             
             if (!validInput) {
-                System.out.println("Sadece sayı kullanın!");
+                System.out.println("Sadece sayı girin!");
                 continue;
+            }
+
+            if (guess == -1) {
+                System.out.println("Oyundan çıkıyorsunuz. Hedef sayı: " + targetNumber);
+                break;
             }
 
             attempts++;
 
             if (guess == targetNumber) {
-                System.out.println("Afferinn toplam " + attempts + " tahminde bildin");
+                System.out.println("Aferin! Toplam " + attempts + " tahminde bildin");
                 guessCorrect = true;
             } else if (guess > targetNumber) {
                 System.out.println("Çok büyük tahmin ettin! daha küçük bir sayı dene");
