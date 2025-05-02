cf064ca4c3691608a652b382589972b06f6db18a
almira mirza <miramirza74@gmail.com>
2025-05-03 02:39:11 +0300

    q2

--- QuestionTwo/BankAccount.java
+++ QuestionTwo/BankAccount.java
@@ -10,3 +10,27 @@ public class BankAccount {
       this.accHolderNum = accHolderNum;
       this.balance = 0; //float mı olsun double mı karar ver 
 }
+
+public void deposit(){
+//(double amount) 
+//Adds the given amount to the balance. Prints a confirmation message
+}
+
+public void withdraw(){
+//(double amount) 
+//Subtracts the given amount from the balance if sufficient 
+//funds are available. If not, prints an "Insufficient Funds" message. 
+
+}
+
+public void getBalance(){
+//: Returns the current balance.
+}
+
+public void displayAccountInfo(){
+//Prints the account number, account holder name, and current balance
+}
+
+
+
+}
