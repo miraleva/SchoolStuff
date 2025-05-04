68ab9f5376670e0386386cf20c58b44d93b835a2
almira mirza <miramirza74@gmail.com>
2025-05-04 13:46:49 +0300

    q1

--- QuestionOne/ArrayCheck.java
+++ QuestionOne/ArrayCheck.java
@@ -6,6 +6,7 @@ public class ArrayCheck {
     public static void main(String[] args) {
         int[] nums = new int[10];
         Random random = new Random();
+
         ArrayList<Integer> evens = new ArrayList<>();
         ArrayList<Integer> odds = new ArrayList<>();
 
@@ -19,7 +20,7 @@ public class ArrayCheck {
         }
 
         System.out.println("Original Array:" + Arrays.toString(nums));
-        System.out.println("Even Count:" + evens.size() + evens);
-        System.out.println("Odd Count:" + odds.size() + odds);
+        System.out.println("Even Count: " + evens.size() +" Even Numbers: "+ evens);
+        System.out.println("Odd Count: " + odds.size() +" Odd Numbers: "+ odds);
     }
 }
