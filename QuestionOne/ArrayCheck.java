4387556d865e70aeee7aab3db738e03cb5c044fe
almira mirza <miramirza74@gmail.com>
2025-05-04 13:01:51 +0300

    q1

---
+++ QuestionOne/ArrayCheck.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArrayCheck {
+    public static void main(String[] args) {
+        int[] nums = new int[10];
+        Random random = new Random();
+        ArrayList<Integer> evens = new ArrayList<>();
+        ArrayList<Integer> odds = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = random.nextInt(100) + 1;
+            if (nums[i] % 2 == 0) {
+                evens.add(nums[i]);
+            } else {
+                odds.add(nums[i]);
+            }
+        }
+
+        System.out.println("Original Array:" + Arrays.toString(nums));
+        System.out.println("Even Count:" + evens.size() + evens);
+        System.out.println("Odd Count:" + odds.size() + odds);
+    }
+}
